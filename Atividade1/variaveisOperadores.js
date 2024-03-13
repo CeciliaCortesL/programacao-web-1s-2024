b4c4659b78dae0f4a12cbb1b945af2209f04b42f
Cecilia Cortes Lopes <105022346+CeciliaCortesL@users.noreply.github.com>
2024-03-13 10:05:59 -0300

    Add files via upload

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,9 @@
+const n1 = 10;
+const n2 = 5;
+
+let c = a + b;
+
+console.log('O resultado da soma entre' +n1+ 'e' +n2+ 'é' +c);
+
+c = a-b;
+console.log(`O resultado da subtração entre ${n1} e ${n2} é ${c}`);

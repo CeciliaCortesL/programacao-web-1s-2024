ca320f9361b42c7c941ec45e9d1529bb322400e7
Cecilia Cortes Lopes <ceciliacorteslopes@gmail.com>
2024-04-09 19:47:11 -0300

    Apaga arquivos atividade 4

--- util/calculadora.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    somar: (a, b) => a + b,
-    subtrair: (a, b) => a - b,
-    multiplicar: (a, b) => a * b,
-    dividir: (a, b) => (b === 0 ? 'Erro: Divisão por 0' : a / b),
-};
